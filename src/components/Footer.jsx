49dabf98c344eaace8db6a565bd338a756be91be
Shalkar Koshenayev <shalkar.koshenayev@gmail.com>
2023-12-30 01:03:39 +0600

    copyright shalkar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,19 @@
 export const Footer = () => {
-  return <footer className="footer container">
-	<p style={{fontFamily: 'Nothing You Could Do, cursive', fontSize: '30px', textAlign: 'center'}}>Made by Bekov Alan<br /> 2022</p>
-	{/*<p style={{fontFamily: 'Nothing You Could Do, cursive', fontSize: '30px', textAlign: 'center'}}>Made by Shalkar Koshenayev <br /> 2022</p>*/}
-  </footer>;
+  const date = new Date(Date.now()).getFullYear();
+  console.log(date);
+  return (
+    <footer className="footer container">
+      <p
+        style={{
+          fontFamily: "Nothing You Could Do, cursive",
+          fontSize: "30px",
+          textAlign: "center",
+        }}
+      >
+        Made by Shalkar Koshenayev
+        <br /> {date}
+      </p>
+      {/*<p style={{fontFamily: 'Nothing You Could Do, cursive', fontSize: '30px', textAlign: 'center'}}>Made by Shalkar Koshenayev <br /> 2022</p>*/}
+    </footer>
+  );
 };
- 
