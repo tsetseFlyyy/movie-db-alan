5b79c353c472454a37731a79676d68fb2815ab24
Shalkar <93659536+tsetseFlyyy@users.noreply.github.com>
2024-10-11 16:42:38 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ export const Footer = () => {
           textAlign: "center",
         }}
       >
-        Made by Bekov Alan
+        Made by someone
         <br /> 2022
       </p>
       {/*<p style={{fontFamily: 'Nothing You Could Do, cursive', fontSize: '30px', textAlign: 'center'}}>Made by Shalkar Koshenayev <br /> 2022</p>*/}
